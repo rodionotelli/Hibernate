0f3ef2583ecd47fc7b13aaeeae632b6926fae414
rodionotelli <148154207+rodionotelli@users.noreply.github.com>
2025-01-13 17:55:51 +0300

    Update Controller.java

--- src/main/java/ru/netology/hibernate/controller/Controller.java
+++ src/main/java/ru/netology/hibernate/controller/Controller.java
@@ -1,6 +1,5 @@
 package ru.netology.hibernate.controller;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +7,7 @@ import ru.netology.hibernate.entity.Person;
 import ru.netology.hibernate.service.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class Controller {
@@ -20,5 +20,15 @@ public class Controller {
     @GetMapping("/persons/by-city")
     public List<Person> getPersons(@RequestParam("city") String city) {
         return service.getPersonsByCity(city);
+    }
+
+    @GetMapping("/persons/by-age")
+    public List<Person> getAge(@RequestParam("age") int age) {
+        return service.getAge(age);
+    }
+
+    @GetMapping("/persons/by-fullname")
+    public Optional<Person> getNameSurname(@RequestParam("name") String name, @RequestParam("surname") String surname) {
+        return service.getNameSurname(name, surname);
     }
 }
